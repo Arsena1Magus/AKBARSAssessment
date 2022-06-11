b055cd55972e13b5a59369f3c4d87848c1afd5cc
Никита Петров <petrovna@mcb.ru>
2022-06-11 13:54:23 +0300

    Изменения пути запроса на получение токена

--- AK BARS Assessment/Constants/Constants.swift
+++ AK BARS Assessment/Constants/Constants.swift
@@ -10,7 +10,7 @@ import Foundation
 enum Constants {
     enum ApiServers {
         static let authServer = "https://invest-auth.msp.akbars.ru/invest_auth/Signin/byUserName/"
-        static let tokenServer = "https://kvalinvesttest.akbf.ru/api/Authenticate/Login"
+        static let tokenServer = "https://kvalinvest.akbf.ru/api/Authenticate/Login"
         static let mainServer = "https://kvalinvest.akbf.ru/api/"
         // static let mainServer =  "https://kvalinvesttest.akbf.ru:442/api/"
     }
